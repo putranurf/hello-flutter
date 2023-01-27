58ded209b13878d6f81c0c90143e2ad79306090d
Putra Nurfajar <putra@pindad.com>
2023-01-27 15:04:43 +0700

    Training Row, Column , Text and Container

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,95 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  // runApp(const RowColumn());
+  // runApp(const TextApp());
+  runApp(const ContainerApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+// class TextApp extends StatelessWidget {
+//   const TextApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: Text("Aplikasi Testing Flutter"),
+//         ),
+//         body: Center(
+//             child: Container(
+//                 color: Colors.lightBlue,
+//                 width: 150,
+//                 height: 100,
+//                 child: Text(
+//                   "Hello World everything is fine and good for any peoples",
+//                   style: TextStyle(
+//                       color: Colors.white,
+//                       fontStyle: FontStyle.italic,
+//                       fontWeight: FontWeight.bold,
+//                       fontSize: 20),
+//                 ))),
+//       ),
+//     );
+//   }
+// }
+
+// class RowColumn extends StatelessWidget {
+//   const RowColumn({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: Text("Latihan"),
+//         ),
+//         body: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           crossAxisAlignment: CrossAxisAlignment.start,
+//           children: <Widget>[
+//             Text("Kolom"),
+//             Text("Kolom2"),
+//             Text("Kolom3"),
+//             Row(
+//               children: <Widget>[
+//                 Text("Kolom4"),
+//                 Text("Kolom5"),
+//                 Text("Kolom6"),
+//               ],
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+class ContainerApp extends StatelessWidget {
+  const ContainerApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Aplikasi Testing Flutter"),
+          title: Text("Container"),
         ),
-        body: Center(child: Text("Hello")),
+        body: Container(
+          color: Colors.red,
+          padding: EdgeInsets.fromLTRB(12, 21, 41, 31),
+          margin: EdgeInsets.all(10),
+          child: Container(
+            margin: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                colors: <Color>[Colors.amber, Colors.blue],
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
