74c40061da401ec064014752ad6512166f60162a
Putra Nurfajar <putra@pindad.com>
2023-01-27 15:27:58 +0700

    Training Stateful Widget

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,8 @@ import 'package:flutter/material.dart';
 void main() {
   // runApp(const RowColumn());
   // runApp(const TextApp());
-  runApp(const ContainerApp());
+  // runApp(const ContainerApp());
+  runApp(const TestStatefulWidget());
 }
 
 // class TextApp extends StatelessWidget {
@@ -65,29 +66,69 @@ void main() {
 //   }
 // }
 
-class ContainerApp extends StatelessWidget {
-  const ContainerApp({super.key});
+// class ContainerApp extends StatelessWidget {
+//   const ContainerApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: Text("Container"),
+//         ),
+//         body: Container(
+//           color: Colors.red,
+//           padding: EdgeInsets.fromLTRB(12, 21, 41, 31),
+//           margin: EdgeInsets.all(10),
+//           child: Container(
+//             margin: EdgeInsets.all(10),
+//             decoration: BoxDecoration(
+//               borderRadius: BorderRadius.circular(20),
+//               gradient: LinearGradient(
+//                 begin: Alignment.topLeft,
+//                 colors: <Color>[Colors.amber, Colors.blue],
+//               ),
+//             ),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+class TestStatefulWidget extends StatefulWidget {
+  const TestStatefulWidget({super.key});
+
+  @override
+  State<TestStatefulWidget> createState() => _TestStatefulWidgetState();
+}
+
+class _TestStatefulWidgetState extends State<TestStatefulWidget> {
+  int number = 0;
+  void onSubmit() => setState(() {
+        number = number + 1;
+      });
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Container"),
+          title: Text("Stateful Widget Demo"),
         ),
-        body: Container(
-          color: Colors.red,
-          padding: EdgeInsets.fromLTRB(12, 21, 41, 31),
-          margin: EdgeInsets.all(10),
-          child: Container(
-            margin: EdgeInsets.all(10),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20),
-              gradient: LinearGradient(
-                begin: Alignment.topLeft,
-                colors: <Color>[Colors.amber, Colors.blue],
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                number.toString(),
+                style: TextStyle(fontSize: 10 + number.toDouble()),
               ),
-            ),
+              ElevatedButton(
+                onPressed: onSubmit,
+                child: Text("Submit"),
+              )
+            ],
           ),
         ),
       ),
